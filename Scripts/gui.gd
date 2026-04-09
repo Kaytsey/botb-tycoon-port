c02619b4acde485a7555fbb3d70e99a34051bd8b
Kaytsey <kevin_arndt@ymail.com>
2026-04-10 00:09:44 +0200

    added lots of text

--- Scripts/gui.gd
+++ Scripts/gui.gd
@@ -4,11 +4,13 @@ extends Control
 
 @export var resources : Label
 @export var flavourtext : Label
+@export var ending_text : Label
 @export var generate_timer : Timer
 @export var cutscene_timer : Timer
 @export var sacrifice_button : Button
 @export var host_battle_button : Button
 @export var restart_button : Button
+@export var buy_n00bs : Button
 
 @export var test_cutscene : Button
 
@@ -27,6 +29,7 @@ var n00b_cost : float = 10.0
 const n00b_cost_base : float = 10.0
 const n00b_cost_mult : float = 1.618
 var entries : int = 0
+var total_entries : int = 0
 var sacrifice_cost : int = 2
 
 const cutscene_area_center : Vector3 = Vector3(0, 0, 0)
@@ -37,6 +40,7 @@ const XHB_cost = {
 	"2HB": 2000,
 	"4HB": 5656,
 	"MAJOR": 56560,
+	"DD2": 5656560, #make number bigger, it's 10 years, man
 }
 
 #progression, ending
@@ -79,7 +83,7 @@ const flavourtext_text = [
 #timestuff
 var timer_iterations : int
 const ticks : float = 0.01
-var tick_speed : float = 0.0
+var tick_speed : float = 1.0
 const flavour_timer : int = 10
 var speed_mult : float = 1.0
 
@@ -104,11 +108,7 @@ func _on_battle_button_up() -> void:
 #Buy n00b-Button
 func _on_n00b_pressed() -> void:
 	make_n00bs()
-	if (n00bs + boon_mult) >= 8000000000:
-		finishable = true
 	update_label_text()
-
-#todo add buy n00bs for boons text
 
 
 #sacrifice n00b-Button
@@ -117,6 +117,9 @@ func _on_sacrifice_n00bs_button_up():
 	update_label_text()
 	sacrifice_button.remove_theme_font_size_override("font_size")
 	sacrifice_button.text = "sacrifice %s n00bs" %(boon_mult*2)
+	if boon_mult >= 8000000000:
+		finishable = true
+		
 #text
 func _on_sacrifice_n_00_bs_mouse_entered():
 	if !sacrificed and n00bs >= 2:
@@ -131,7 +134,6 @@ func _on_sacrifice_n_00_bs_mouse_exited():
 
 #host bat0l
 func _on_host_battle_pressed():
-	hide_all()
 	host_xhb()
 
 func _on_restart_pressed():
@@ -139,22 +141,60 @@ func _on_restart_pressed():
 
 func _on_test_cutscene_pressed():
 	play_cutscene()
+	cutscene_timer.start()
 
 #Buttons End
 
 #General Text
 func update_label_text() -> void:
+	#OHB hosting
+	#this is messy lol
+	if boons <= XHB_cost["OHB"]:
+		host_battle_button.text = "Hosting OHB costs %s" %XHB_cost["OHB"] + " boons"
+	if boons >= XHB_cost["OHB"] and boons < XHB_cost["2HB"]:
+		host_battle_button.text = "Host OHB for %s" %XHB_cost["OHB"] + " boons"
+	if boons >= XHB_cost["2HB"] and boons < XHB_cost["4HB"]:
+		host_battle_button.text = "Host 2HB for %s" %XHB_cost["2HB"] + " boons"
+	if boons >= XHB_cost["4HB"] and boons < XHB_cost["MAJOR"]:
+		host_battle_button.text = "Host 4HB for %s" %XHB_cost["4HB"] + " boons"
+	if boons >= XHB_cost["MAJOR"] and boons < XHB_cost["DD2"]:
+		host_battle_button.text = "Host MAJOR for %s" %XHB_cost["MAJOR"] + " boons"
+	if boons >= XHB_cost["DD2"]:
+		host_battle_button.text = "Host DD2 for %s" %XHB_cost["DD2"] + " boons"
+	
+	#General boons
 	if !n00bs:
 		resources.text = "%.2f boons" %boons
-	else:
+	if n00bs:
 		resources.text = "%.2f boons & " %boons + "%s n00bs " %n00bs
+	buy_n00bs.text = "buy n00b for %s boons" %n00b_cost
+	
 
 
 func update_flavourtext_text() -> void:
 	if sacrificed:
-		flavourtext.text = flavourtext_text[randf_range(-8, -1)]
+		flavourtext.text = flavourtext_text[randi_range(-8, -1)]
 	else:
-		flavourtext.text = flavourtext_text[randf_range(progression, 5 + progression)]
+		flavourtext.text = flavourtext_text[randi_range(progression, 5 + progression)]
+
+func show_ending_text() -> void:
+	#make text appear gradual
+	ending_text.visible_ratio = 0
+	if sacrificed:
+		ending_text.text = "Your greed doomed all of humanity.
+			Everyone has been sacrificed and nobody remains to listen.
+			You made Hell a real place."
+	
+	else:
+		ending_text.text = "You succeeded in building a comfy community!
+			Everyone rejoices in harmony!
+			Praise be upon you!"
+	ending_text.show()
+	
+	#make text appear gradual
+	var tween : Tween = create_tween()
+	tween.tween_property(ending_text, "visible_ratio", 1, 5)
+	
 
 
 #timers
@@ -171,8 +211,18 @@ func _on_timer_timeout() -> void:
 	generate_timer.start(ticks)
 
 func _on_cutscene_timer_timeout():
+	if boons >= XHB_cost["DD2"]:
+		finishable = true
+		for slug in get_tree().get_nodes_in_group("slugs"):
+			slug.queue_free()
+		return
+		
 	show_all()
 	restart_button.hide()
+	ending_text.hide()
+	#remove slugs
+	for slug in get_tree().get_nodes_in_group("slugs"):
+		slug.queue_free()
 
 #resources
 func make_boons() -> void:
@@ -212,56 +262,75 @@ func calculate_n00b_cost() -> float:
 	else:
 		return n00b_cost_base * pow(n00b_cost_mult, n00bs)
 
-#todo
+#TODO
 #i will not write an audio engine for this, sorry
 func play_sfx() -> void:
 	
 	pass
 
-
 #todo
-#todo
-#todo
-#maybe have an actual return value
-#maybe make node outside of this
 func play_cutscene() -> void:
-	#spawn slugs randomly in visible field (for-loop, random amount)
-	#have them have random animations
-	#tally points
+	tally_points()
 	#after x seconds go back
 	var slug_amount = randi_range(1, min(n00bs, 560))
 	for i in slug_amount:
-		#print(i)
 		var slug = slug_scene.instantiate()
+		slug.add_to_group("slugs")
 		get_tree().current_scene.add_child(slug)
 		slug.global_position = cutscene_area_center + Vector3(
 			randf_range(-4.0, 4.0),
 			randf_range(-3.0, 3.0),
 			randf_range(-10.0, 10.0))
-		#var animations: AnimationPlayer = slug.get_node("AnimationPlayer")
-		#var animation = animations.get_animation_list()
-		#animation.play(animations.pick_random())
+		slug.rotation.y = randf_range(0, 360)
+		
+		var animations: AnimationPlayer = slug.get_node("AnimationPlayer")
+		var animation = animations.get_animation_list()
+		animations.play(animation[randi() % animation.size()])
+		#random animation speed TODO add general speedup at breakpoints
+		animations.set_speed_scale(randf_range(0.5 * tick_speed, 2 * tick_speed))
+		
+
+func tally_points() -> void:
+	#
 	
+	pass
 
 
 #todo
 func host_xhb() -> void:
 	if boons >= XHB_cost["OHB"] and boons < XHB_cost["2HB"]:
+		hide_all()
+		cutscene_timer.wait_time = 5
 		cutscene_timer.start()
 		play_cutscene()
 
 	if boons >= XHB_cost["2HB"] and boons < XHB_cost["4HB"]:
+		hide_all()
+		cutscene_timer.wait_time = 10
 		cutscene_timer.start()
 		play_cutscene()
 
 	if boons >= XHB_cost["4HB"] and boons < XHB_cost["MAJOR"]:
+		hide_all()
+		#cutscene_timer.wait_time = 20
 		cutscene_timer.start()
 		play_cutscene()
 	
 	#MAJOR can have more entries than n00bs
-	if boons >= XHB_cost["MAJOR"]:
+	if boons >= XHB_cost["MAJOR"] and boons < XHB_cost["DD2"]:
+		hide_all()
+		#cutscene_timer.wait_time = 30
 		cutscene_timer.start()
 		play_cutscene()
+		tick_speed *= 1.1
+		
+	#similar to major
+	if boons >= XHB_cost["DD2"]:
+		hide_all()
+		#cutscene_timer.wait_time = 120
+		cutscene_timer.start()
+		play_cutscene()
+		#win game maybe
 
 #todo
 func win_game() -> void:
@@ -270,6 +339,7 @@ func win_game() -> void:
 		hide_all()
 		particles.get_node("Ascend").show()
 		$TrueEnd.play()
+		show_ending_text()
 		restart_fadein()
 		
 		finishable = false
@@ -279,6 +349,7 @@ func win_game() -> void:
 		hide_all()
 		particles.get_node("Descend").show()
 		$End.play()
+		show_ending_text()
 		restart_fadein()
 		
 		finishable = false
@@ -290,7 +361,8 @@ func restart_fadein() -> void:
 	tween.tween_interval(13.0)
 	tween.tween_property(restart_button, "modulate:a", 1.0, 5.0)
 	
-	
+
+
 
 #hides all hideable children
 func hide_all() -> void:
@@ -314,7 +386,7 @@ func restart_game() -> void: #resets all relevant variables
 	sacrificed = false
 	progression = 0
 	finishable = false
-	tick_speed = 0.0
+	tick_speed = 1.0
 	speed_mult = 1.0
 	
 	$End.stop()
@@ -324,4 +396,5 @@ func restart_game() -> void: #resets all relevant variables
 	particles.get_node("Ascend").hide()
 	
 	show_all()
+	ending_text.hide()
 	restart_button.hide()
